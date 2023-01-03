e07b31d69eb41bc15112b401cf6820b173a20f8c
無免ライダー <106133707+mumenrider21@users.noreply.github.com>
2023-01-03 15:15:50 +0900

    Add files via upload

--- portfolios/app01/main.js
+++ portfolios/app01/main.js
@@ -1,35 +1,4 @@
 
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
 // スクロール量を取得する要素を取得
 var scrollElm = (function() {
   if('scrollingElement' in document) {
@@ -64,3 +33,95 @@ for(var i = 0; sections.length > i; i++) {
 window.addEventListener('scroll', function() {
   scaler.style.transform = 'translateZ(' + scrollElm.scrollTop / 100 + 'px)';
 });
+
+
+var particleAlphabet = {
+	Particle: function(x, y) {
+		this.x = x;
+		this.y = y;
+		this.radius = 3.5;
+		this.draw = function(ctx) {
+			ctx.save();
+			ctx.translate(this.x, this.y);
+			ctx.fillStyle = 'white';
+			ctx.fillRect(0, 0, this.radius, this.radius);
+			ctx.restore();
+		};
+	},
+	init: function() {
+		particleAlphabet.canvas = document.querySelector('canvas');
+		particleAlphabet.ctx = particleAlphabet.canvas.getContext('2d');
+		particleAlphabet.W = window.innerWidth;
+		particleAlphabet.H = window.innerHeight;
+		particleAlphabet.particlePositions = [];
+		particleAlphabet.particles = [];
+		particleAlphabet.tmpCanvas = document.createElement('canvas');
+		particleAlphabet.tmpCtx = particleAlphabet.tmpCanvas.getContext('2d');
+
+		particleAlphabet.canvas.width = particleAlphabet.W;
+		particleAlphabet.canvas.height = particleAlphabet.H;
+
+		setInterval(function(){
+			particleAlphabet.changeLetter();
+			particleAlphabet.getPixels(particleAlphabet.tmpCanvas, particleAlphabet.tmpCtx);
+		}, 1200);
+
+		particleAlphabet.makeParticles(1000);
+		particleAlphabet.animate();
+	}, 
+	currentPos: 0,
+	changeLetter: function() {
+		var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ',
+			letters = letters.split('');
+		particleAlphabet.time = letters[particleAlphabet.currentPos];
+		particleAlphabet.currentPos++;
+		if (particleAlphabet.currentPos >= letters.length) {
+			particleAlphabet.currentPos = 0;
+		}
+	},
+	makeParticles: function(num) {
+		for (var i = 0; i <= num; i++) {
+			particleAlphabet.particles.push(new particleAlphabet.Particle(particleAlphabet.W / 2 + Math.random() * 400 - 200, particleAlphabet.H / 2 + Math.random() * 400 -200));
+		}
+	},
+	getPixels: function(canvas, ctx) {
+		var keyword = particleAlphabet.time,
+			gridX = 6,
+			gridY = 6;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		ctx.fillStyle = 'red';
+		ctx.font = 'italic bold 330px Noto Serif';
+		ctx.fillText(keyword, canvas.width / 2 - ctx.measureText(keyword).width / 2, canvas.height / 2 + 100);
+		var idata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		var buffer32 = new Uint32Array(idata.data.buffer);
+		if (particleAlphabet.particlePositions.length > 0) particleAlphabet.particlePositions = [];
+		for (var y = 0; y < canvas.height; y += gridY) {
+			for (var x = 0; x < canvas.width; x += gridX) {
+				if (buffer32[y * canvas.width + x]) {
+					particleAlphabet.particlePositions.push({x: x, y: y});
+				}
+			}
+		}
+	},
+	animateParticles: function() {
+		var p, pPos;
+		for (var i = 0, num = particleAlphabet.particles.length; i < num; i++) {
+			p = particleAlphabet.particles[i];
+			pPos = particleAlphabet.particlePositions[i];
+			if (particleAlphabet.particles.indexOf(p) === particleAlphabet.particlePositions.indexOf(pPos)) {
+			p.x += (pPos.x - p.x) * .3;
+			p.y += (pPos.y - p.y) * .3;
+			p.draw(particleAlphabet.ctx);
+		}
+		}
+	},
+	animate: function() {
+		requestAnimationFrame(particleAlphabet.animate);
+		particleAlphabet.ctx.fillStyle = 'rgba(23, 41, 58, .8)';
+		particleAlphabet.ctx.fillRect(0, 0, particleAlphabet.W, particleAlphabet.H);
+		particleAlphabet.animateParticles();
+	}
+};
+
+window.onload = particleAlphabet.init;
--- portfolios/app02/main.js
+++ portfolios/app02/main.js
@@ -39,3 +39,80 @@ function FixedAnime() {
   $(window).on('load', function () {
 	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
   });
+
+
+
+
+
+
+
+
+  const fixedElm = document.getElementById('header');
+let scrollPoint = 0; // 現在のスクロール位置をセットする変数
+let lastPoint = 0; // 1つ前のスクロール位置をセットする変数
+
+window.addEventListener("scroll",function(){
+  
+  scrollPoint = window.scrollY;
+  
+  if(scrollPoint > lastPoint){ // 下スクロールの場合
+    fixedElm.classList.add('fixed-hide');
+  }else{ // 上スクロールの場合
+    fixedElm.classList.remove('fixed-hide');
+  }
+  
+  lastPoint = scrollPoint;
+});
+
+
+
+
+
+var beforePos = 0;//スクロールの値の比較用の設定
+
+//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
+function ScrollAnime() {
+    var elemTop = $('#area-2').offset().top;//#area-2の位置まできたら
+  var scroll = $(window).scrollTop();
+    //ヘッダーの出し入れをする
+    if(scroll == beforePos) {
+    //IE11対策で処理を入れない
+    }else if(elemTop > scroll || 0 > scroll - beforePos){
+    //ヘッダーが上から出現する
+    $('#header').removeClass('UpMove'); //#headerにUpMoveというクラス名を除き
+    $('#header').addClass('DownMove');//#headerにDownMoveのクラス名を追加
+    }else {
+    //ヘッダーが上に消える
+        $('#header').removeClass('DownMove');//#headerにDownMoveというクラス名を除き
+    $('#header').addClass('UpMove');//#headerにUpMoveのクラス名を追加
+    }
+    
+    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
+}
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
+});
+
+
+
+
+//リンク先のidまでスムーススクロール
+//※ページ内リンクを行わない場合は不必要なので削除してください
+    var headerH = $("#header").outerHeight(true);//headerの高さを取得    
+    $('#g-navi li a').click(function () {
+  var elmHash = $(this).attr('href'); 
+  var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さまでスクロール
+  $('body,html').animate({scrollTop: pos}, 1000);
+  return false;
+});
+
+
+
